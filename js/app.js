86a5071211eb8174268935e1f6d61375ff83bdb4
geleeroyale <krassvs@gmail.com>
2017-12-29 03:53:36 +0100

    Init

--- js/app.js
+++ js/app.js
@@ -1,15 +1,9 @@
+/*jshint esversion: 6 */
+
 /*
- * Create a list that holds all of your cards - because this is a memory game, all cards exist twice
+ * Create a list that holds all the cards
  */
-const allCards = ['fa-anchor',
-                  'fa-bicycle',
-                  'fa-bomb',
-                  'fa-bolt',
-                  'fa-cube',
-                  'fa-diamond',
-                  'fa-leaf',
-                  'fa-paper-plane-o',
-                  'fa-anchor',
+const baseSet = ['fa-anchor',
                   'fa-bicycle',
                   'fa-bomb',
                   'fa-bolt',
@@ -17,17 +11,31 @@ const allCards = ['fa-anchor',
                   'fa-diamond',
                   'fa-leaf',
                   'fa-paper-plane-o'];
+/**
+ * Cards need to be doubled - this is memory!
+ */
+const allCards = [...baseSet, ...baseSet];
+
 
 let counter = 0;
-const openCards = [];
+let openCards = [];
+let lockedCards = [];
 const gameboard = document.querySelector('#gameboard');
-const timerStart = new Date();
-let card1;
-let card2;
+const movesDisplay = document.querySelector('#moves');
+const ratingDisplay = document.querySelector('#rating');
+let move = 0;
+let rating = 3;
+let deltaTime = 0;
+let lastCard = '';
+let match = false;
+
 
 function moveCounter() {
-  const halfmoves = 0;
-  const moves = 0;
+  move++;
+}
+
+function renderMoveCounter() {
+  movesDisplay.textContent = move;
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -59,43 +67,84 @@ function displayCards () {
     }
 }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+/**
+ * This section is setting up the board
  */
+
+restartIcon = document.querySelector('#restart');
+restartIcon.addEventListener('click', restart);
+shuffle(allCards);
+displayCards();
+let timerStart = new Date();
 const cards = document.querySelectorAll('.card');
 
-function cardClicked() {
-            this.classList.add('open', 'show');
-            addOpenCards(this.firstChild.className);
-            counter += 1;
-            card1 = this.firstChild;
-        }
-        else if (counter === 1) {
-            this.classList.add('open', 'show');
-            addOpenCards(this.firstChild.className);
-            card2 = this.firstChild;
-            counter = 0;
 
-        }
+
+function cardClicked() {
+    card = this;
+    showCard(card);
+    addOpenCards(card.innerHTML);
+    if (openCards.length === 2) {
+      matchCards(card);
+    }
+    else if (openCards.length === 1) {
+      lastCard = this;
+    }
+    else {
+      removeOpenCards();
+    }
+    checkWinCondition();
+}
+
+function checkWinCondition() {
+  if (lockedCards.length === 8){
+    calculateDeltaTime();
+    delayStep(200, showScoreboard());
+  }
+}
+
+function delayStep(func, time) {
+  timeoutID = window.setTimeout(func, time);
+}
+
+function lockCards() {
+    lockedCards.push(openCards);
 }
 
 function matchCards() {
-  if (card1.className == card2.className && card1 != card2) {
-      console.log('Hussah');
+  //To find if a value or element exists in an array
+  if (openCards[0] === openCards[1] && card !== lastCard) {
+    isMatch();
   }
+  //To find if a value or element DOES NOT exist in an array
+  else if (openCards[0] != openCards[1] && card != lastCard) {
+    delayStep(isNotMatch, 200);
+  }
+}
 
-  else {
-    card1.classList.remove('open', 'show');
-    removeOpenCards(card1);
-    removeOpenCards(card2);
-  }
+function isMatch() {
+  console.log('Match!');
+  lastCard.classList.add('match');
+  card.classList.add('match');
+  lockCards();
+  removeOpenCards(lastCard);
+  removeOpenCards(card);
+  lastCard.removeEventListener('click', cardClicked);
+  card.removeEventListener('click', cardClicked);
+  moveCounter();
+  renderMoveCounter();
+  updateRating();
+}
+
+function isNotMatch() {
+  console.log('No Match!');
+  removeOpenCards(lastCard);
+  removeOpenCards(card);
+  hideCard(lastCard);
+  hideCard(card);
+  moveCounter();
+  renderMoveCounter();
+  updateRating();
 }
 
 function addOpenCards(card) {
@@ -103,27 +152,74 @@ function addOpenCards(card) {
 }
 
 function removeOpenCards(card) {
-  openCards.pop(card);
+  openCards = [];
 }
 
-shuffle(allCards);
-displayCards();
-
-/*
-var myCustomDiv = document.createElement('div');
-
-function respondToTheClick() {
-    console.log('A paragraph was clicked.');
+function hideCard(card) {
+  card.classList.remove('open', 'show');
+  lastCard.classList.remove('open', 'show');
+  card.addEventListener('click', cardClicked);
 }
 
-for (var i = 1; i <= 200; i++) {
-    var newElement = document.createElement('p');
-    newElement.textContent = 'This is paragraph number ' + i;
 
-    myCustomDiv.appendChild(newElement);
+function showCard(card) {
+  card.classList.add('open', 'show');
+  card.removeEventListener('click', cardClicked);
 }
 
-myCustomDiv.addEventListener('click', respondToTheClick);
+function calculateDeltaTime() {
+    let timerStop = new Date() - timerStart;
+    deltaTime = timerStop/1000;
+    return deltaTime;
+}
 
-document.body.appendChild(myCustomDiv);
-*/
+function updateRating() {
+  if (move < 5) {
+    rating = 3;
+    ratingDisplay.innerHTML = '<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>';
+  }
+  else if (move < 10) {
+    rating = 2;
+    ratingDisplay.innerHTML = '<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>';
+  }
+  else if (move < 20) {
+    rating = 1;
+    ratingDisplay.innerHTML = '<li><i class="fa fa-star"></i></li>';
+  }
+  else if (move >= 20) {
+    rating = 0;
+    ratingDisplay.innerHTML = '';
+  }
+}
+
+function resetRating() {
+  rating = 3;
+  ratingDisplay.innerHTML = '<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>';
+}
+
+function showScoreboard() {
+  if (window.confirm('You have won! Moves needed: ' + move + '. Time to clear board: ' + deltaTime + ' seconds.')) {
+  restart();
+}
+}
+
+function clearGameboard() {
+  gameboard.innerHTML = '';
+}
+
+function restart() {
+  counter = 0;
+  openCards = [];
+  lockedCards = [];
+  move = 0;
+  deltaTime = 0;
+  lastCard = '';
+  match = false;
+
+  resetRating();
+  clearGameboard();
+  renderMoveCounter();
+  shuffle(allCards);
+  displayCards();
+  timerStart = new Date();
+}

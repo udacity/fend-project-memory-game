413dba2b90c2614fc76dd03eb7ebe07ea6f8aa63
Jeremy Levett <jezlevett@gmail.com>
2018-02-12 16:25:19 +1100

    Added basic js functionality.
     TO DO:
     Add Modal
     Add select using arrow keys and select with enter.
     Add reset using letter 'r'
     Save highscores

--- js/app.js
+++ js/app.js
@@ -1,19 +1,159 @@
-/*
- * Create a list that holds all of your cards
- */
+
+const restartElement=document.querySelector('.restart');
+const deckElement = document.querySelector('.deck');
+const fragment = document.createDocumentFragment();
+const deckNodeList = document.querySelectorAll('.card');/*Create a list that holds all of your cards*/
+const cardArray = [];
+
+let moves=0;
+let matchedcards =0;	
+let timeNow=0;
+let timerOn = true;
+let cardWrongAnimation =true;
+let openCardList = [];
+let randomDeckArray;
 
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+
+/* Intial funciton: Display the randomized cards on the page*/
+(function(){ 
+for(card of deckNodeList)
+	cardArray.push(card); 
+randomDeckArray=shuffle(cardArray);//Shuffle the list of cards using the provided "shuffle" method below
+for(randomCard of randomDeckArray)//Loop through each card and create its HTML
+    fragment.appendChild(randomCard);
+deckElement.appendChild(fragment); //Add each card's HTML to the page
+}());
+
+ /*EventListeners*/
+deckElement.addEventListener('click', cardClicked, false);//If a card is clicked
+restartElement.addEventListener('click', restartGame, false);//If reset is pressed*
+
+function restartGame(){
+	//Clear selected List
+	openCardList = [];
+	//Timer restart
+	timeNow=0;
+	timerNode.innerHTML = 0;
+	//Star restart
+	for(star of starsNodeList)
+		star.style.display='inline-block';
+	//Moves restart
+	moves =0;
+	document.querySelector('.moves').innerHTML= moves;
+	//Shuffle cards and replace 
+	randomDeckArray=shuffle(cardArray);
+	for(randomCard of randomDeckArray){
+		randomCard.className="card";
+	    fragment.appendChild(randomCard);
+	}
+	deckElement.appendChild(fragment); 
+}
+
+function cardClicked(event){
+		let cardSelected = event.path[0];
+		if(cardSelectedNotMatchedYet(cardSelected)){
+			if(addToOpenCardList(cardSelected)){
+				if(doCardsMatch()){//Lock cards into the open position
+					bothCardsNowInOpen();
+					matchedcards++;
+				}else	
+					cardsBacktoHidden();
+					updateMoves();
+					updateStars(); 
+					checkAllCardsMatched();
+			}
+		}
+}	
+
+/*Update the moves variable and display on screen*/
+function updateMoves(){
+	moves++;
+	document.querySelector('.moves').innerHTML= moves;
+}
+/*Updates the number of stars which depends on the score"*/
+const starsNodeList = document.querySelectorAll('.fa-star');
+ function updateStars(){
+	if(moves===10)
+		starsNodeList[0].style.display='none';
+	if(moves===16)
+		starsNodeList[1].style.display='none';
+}
+
+
+/*Checks if all the cards are matched*/
+function checkAllCardsMatched(){
+	if(matchedcards==1){ //Change to 8 later on
+		console.log("Gamer over");
+		timerOn = false;
+
+	}
+		
+}
+
+
+/*Display card if card is not already matched and the card was clicked*/
+function cardSelectedNotMatchedYet(node){
+	if(node.className!="card match animated pulse" && node.className!="card match"&&node.nodeName=="LI"&&cardWrongAnimation){
+		node.className="card open show";
+		return true;
+	}
+	return false;
+} 
+
+/*Add the card to a *list* of "open" cards. If list is full return true, if list is not full return false*/
+function addToOpenCardList(node){
+			openCardList.push(node);
+			if(openCardList[0]==openCardList[1])
+				openCardList.pop(openCardList[1]);
+			if(openCardList.length ==2)
+				return true;
+			else
+				return false;
+}
+
+/*If selected list cards match, return true, if not return false*/
+function doCardsMatch(){
+	if(openCardList[0].innerHTML===openCardList[1].innerHTML)
+		return true;
+	return false;
+}	 
+
+/*If cards are not in the correct position, play wrong cardanimation,
+hide cards and temporaily disable mouse clicks*/
+function cardsBacktoHidden(){
+	openCardList[0].className="card wrong show animated shake";
+	openCardList[1].className="card wrong show animated shake";
+	cardWrongAnimation = false;
+	setTimeout(
+		function(){
+			openCardList[0].className="card";
+			openCardList[1].className="card";
+			openCardList = [];
+			cardWrongAnimation = true;
+		},600);
+}
+
+/*If the cards do match, lock the cards in the open position*/
+function bothCardsNowInOpen(){
+	openCardList[0].className="card match animated pulse"
+	openCardList[1].className="card match animated pulse";
+	openCardList = [];
+}
+
+
+/*Stopwatch function*/
+const timerNode = document.querySelector('.timer');
+setInterval(function(){ 
+	if(timerOn) 
+		timerNode.innerHTML = timeNow++;
+}, 1000);
+
+/*Shuffle function*/
+//From http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -21,18 +161,11 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+// TO DO:
+// Add Modal
+// Add select using arrow keys and select with enter.
+// Add reset using letter 'r'
+// Save highscores 

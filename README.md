6a2e5d778eef223d513e9e80f0e67b55e6d6439a
Jeremy Levett <jezlevett@gmail.com>
2018-02-13 08:21:56 +1100

    Complete game with modal and required functionality.
    No Known bugs
    
    Future improvements:
    -High Score Table
    -Use arrow keys and enter to play
    -Save game state and high score table

--- README.md
+++ README.md
@@ -3,18 +3,12 @@
 ## Table of Contents
 
 * [Instructions](#instructions)
-* [Contributing](#contributing)
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+Open up index.html in your browser and enjoy.
 
 ## Contributing
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
-
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+This repository has the starter code from udacity.
+This included basic static css and html code which providing layout, style and card symbols for the main game screen.
--- js/app.js
+++ js/app.js
@@ -1,153 +1,174 @@
 
-const restartElement=document.querySelector('.restart');
+/*Global Variables*/
+const restartElement = document.querySelector('.restart');
 const deckElement = document.querySelector('.deck');
 const fragment = document.createDocumentFragment();
-const deckNodeList = document.querySelectorAll('.card');/*Create a list that holds all of your cards*/
+const deckNodeList = document.querySelectorAll('.card');/*Create a list that holds all cards*/
+const modal = document.querySelector('.modal-background');
+const restartImgElement = document.querySelector('.restart-img');
+const moveList = document.querySelectorAll('.moves');
+const starsNodeList = document.querySelectorAll('.fa-star');
+const timerList = document.querySelectorAll('.timer');
 const cardArray = [];
 
-let moves=0;
-let matchedcards =0;	
-let timeNow=0;
-let timerOn = true;
-let cardWrongAnimation =true;
+let gamePlay = {
+	moves:0,
+	matchedcards:0,
+	timeNow:0,
+	timerOn:true,
+	cardWrongAnimation:true
+};
+
 let openCardList = [];
 let randomDeckArray;
 
-
-
-
-/* Intial funciton: Display the randomized cards on the page*/
+/* Intial function: Display the randomized hidden cards on the page*/
 (function(){ 
-for(card of deckNodeList)
-	cardArray.push(card); 
-randomDeckArray=shuffle(cardArray);//Shuffle the list of cards using the provided "shuffle" method below
-for(randomCard of randomDeckArray)//Loop through each card and create its HTML
-    fragment.appendChild(randomCard);
-deckElement.appendChild(fragment); //Add each card's HTML to the page
+	for(card of deckNodeList)
+		cardArray.push(card);
+	randomizeDeck();
 }());
 
- /*EventListeners*/
+/*EventListeners*/
 deckElement.addEventListener('click', cardClicked, false);//If a card is clicked
-restartElement.addEventListener('click', restartGame, false);//If reset is pressed*
+restartElement.addEventListener('click', restartGame, false);//If restart symbol on main screen is pressed
+restartImgElement.addEventListener('click', restartGamefromModal, false);//If restart Image is pressed
 
+/*Restart the game from the modal*/
+function restartGamefromModal(){
+	restartImgElement.className ='restart-img animated rotateOut';
+	setTimeout(function(){
+		modal.style.display = 'none';
+		restartGame();
+		restartImgElement.className ='restart-img';
+		gamePlay.timerOn = true;
+	},500);
+}
+
+/*Restart game from main screen restart symbol*/
 function restartGame(){
-	//Clear selected List
-	openCardList = [];
-	//Timer restart
-	timeNow=0;
-	timerNode.innerHTML = 0;
-	//Star restart
-	for(star of starsNodeList)
-		star.style.display='inline-block';
-	//Moves restart
-	moves =0;
-	document.querySelector('.moves').innerHTML= moves;
-	//Shuffle cards and replace 
-	randomDeckArray=shuffle(cardArray);
+	openCardList = [];//Clear selected List
+	gamePlay.timeNow = 0;//Timer restart
+	timerList[0].innerHTML = 0;
+	for(star of starsNodeList)//Stars restart
+		star.style.display ='inline-block';
+	gamePlay.moves = 0;//Moves restart
+	gamePlay.matchedcards = 0;//Reset matched cards
+	document.querySelector('.moves').innerHTML = gamePlay.moves;	//Reset main screen move counter to 0.
+	randomizeDeck();
+}
+
+/*Randomize and hide the deck*/
+function randomizeDeck(){
+	randomDeckArray = shuffle(cardArray);//Randomize array ie cards
 	for(randomCard of randomDeckArray){
-		randomCard.className="card";
-	    fragment.appendChild(randomCard);
+		randomCard.className = 'card';//Hide all cards
+	    fragment.appendChild(randomCard);//Attach card from randomized deck to a fragment
 	}
-	deckElement.appendChild(fragment); 
+	deckElement.appendChild(fragment);//attach fragment to deck
 }
 
+/*Game logic to follow if a card is clicked*/
 function cardClicked(event){
-		let cardSelected = event.path[0];
-		if(cardSelectedNotMatchedYet(cardSelected)){
-			if(addToOpenCardList(cardSelected)){
-				if(doCardsMatch()){//Lock cards into the open position
-					bothCardsNowInOpen();
-					matchedcards++;
-				}else	
-					cardsBacktoHidden();
-					updateMoves();
-					updateStars(); 
-					checkAllCardsMatched();
-			}
+	let cardSelected = event.path[0];
+	if(cardSelectedNotMatchedYet(cardSelected)){
+		if(addToOpenCardList(cardSelected)){
+			if(doCardsMatch()){//If cards match lock cards into the open position
+				bothCardsNowInOpen();
+				gamePlay.matchedcards++;//tracks how many cards have been matched
+			}else	
+				cardsBacktoHidden();
+			updateMoves();
+			updateStars();
+			checkAllCardsMatched();
 		}
-}	
+	}
+}
 
-/*Update the moves variable and display on screen*/
+/*Update the number of 'moves' variable and display on screen*/
 function updateMoves(){
-	moves++;
-	document.querySelector('.moves').innerHTML= moves;
-}
-/*Updates the number of stars which depends on the score"*/
-const starsNodeList = document.querySelectorAll('.fa-star');
- function updateStars(){
-	if(moves===10)
-		starsNodeList[0].style.display='none';
-	if(moves===16)
-		starsNodeList[1].style.display='none';
+	gamePlay.moves++;
+	for(element of moveList)
+		element.innerHTML = gamePlay.moves;
 }
 
+/*Updates the number of 'stars' which depends on the score"*/
+ function updateStars(){
+	if(gamePlay.moves === 10){
+		starsNodeList[0].style.display = 'none';
+		starsNodeList[3].style.display = 'none';
+	}else if(gamePlay.moves === 16){
+		starsNodeList[1].style.display = 'none';
+		starsNodeList[4].style.display = 'none';
+	}
+}
 
 /*Checks if all the cards are matched*/
 function checkAllCardsMatched(){
-	if(matchedcards==1){ //Change to 8 later on
-		console.log("Gamer over");
-		timerOn = false;
-
+	if(gamePlay.matchedcards === 8){
+		gamePlay.timerOn  = false;
+		setTimeout(function(){
+			modal.style.display = 'block';
+		},800);
 	}
-		
 }
 
-
-/*Display card if card is not already matched and the card was clicked*/
+/*Display card, if card is not already matched and the card was clicked*/
 function cardSelectedNotMatchedYet(node){
-	if(node.className!="card match animated pulse" && node.className!="card match"&&node.nodeName=="LI"&&cardWrongAnimation){
-		node.className="card open show";
+	if(node.className != 'card match animated pulse' && node.className != 'card match' && node.nodeName == 'LI' && gamePlay.cardWrongAnimation){
+		node.className = 'card open show';
 		return true;
 	}
 	return false;
-} 
+}
 
 /*Add the card to a *list* of "open" cards. If list is full return true, if list is not full return false*/
 function addToOpenCardList(node){
-			openCardList.push(node);
-			if(openCardList[0]==openCardList[1])
-				openCardList.pop(openCardList[1]);
-			if(openCardList.length ==2)
-				return true;
-			else
-				return false;
+	openCardList.push(node);
+	if(openCardList[0] == openCardList[1])
+		openCardList.pop(openCardList[1]);
+	if(openCardList.length == 2)
+		return true;
+	else
+		return false;
 }
 
 /*If selected list cards match, return true, if not return false*/
 function doCardsMatch(){
-	if(openCardList[0].innerHTML===openCardList[1].innerHTML)
+	if(openCardList[0].innerHTML === openCardList[1].innerHTML)
 		return true;
 	return false;
-}	 
+}
 
-/*If cards are not in the correct position, play wrong cardanimation,
-hide cards and temporaily disable mouse clicks*/
+/*If cards are not in the correct position, play wrong card animation,
+hide cards and temporaily disable mouse clicks so mo error occurs during wrong animation play*/
 function cardsBacktoHidden(){
-	openCardList[0].className="card wrong show animated shake";
-	openCardList[1].className="card wrong show animated shake";
-	cardWrongAnimation = false;
+	openCardList[0].className = 'card wrong show animated shake';
+	openCardList[1].className = 'card wrong show animated shake';
+	gamePlay.cardWrongAnimation = false;
 	setTimeout(
 		function(){
-			openCardList[0].className="card";
-			openCardList[1].className="card";
+			openCardList[0].className = 'card';
+			openCardList[1].className = 'card';
 			openCardList = [];
-			cardWrongAnimation = true;
+			gamePlay.cardWrongAnimation = true;
 		},600);
 }
 
 /*If the cards do match, lock the cards in the open position*/
 function bothCardsNowInOpen(){
-	openCardList[0].className="card match animated pulse"
-	openCardList[1].className="card match animated pulse";
+	openCardList[0].className = 'card match animated pulse';
+	openCardList[1].className = 'card match animated pulse';
 	openCardList = [];
 }
 
-
 /*Stopwatch function*/
-const timerNode = document.querySelector('.timer');
-setInterval(function(){ 
-	if(timerOn) 
-		timerNode.innerHTML = timeNow++;
+setInterval(function(){
+	if(gamePlay.timerOn ){
+		for(element of timerList)
+			element.innerHTML = gamePlay.timeNow;
+		gamePlay.timeNow++;
+	}
 }, 1000);
 
 /*Shuffle function*/
@@ -163,9 +184,3 @@ function shuffle(array) {
     }
     return array;
 }
-
-// TO DO:
-// Add Modal
-// Add select using arrow keys and select with enter.
-// Add reset using letter 'r'
-// Save highscores 

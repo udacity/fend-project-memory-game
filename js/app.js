8e68c1c2311b36a842d172a0723fff745dc807b1
Nadish Sood <nadishsood@gmail.com>
2018-04-05 19:49:26 +0530

    Added basic functionality to game !

--- js/app.js
+++ js/app.js
@@ -1,14 +1,42 @@
 /*
- * Create a list that holds all of your cards
- */
+ * Create a list that holds all of your cardsshow
+*/
+
+var begin = function(){
+
+	var moves = 0;
+	var starHtml = `<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>`;
+	$(".stars").html(starHtml);
 
 
+//get all li elements of deck of cards
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+
+	var li = $(".card").children();
+
+	var iClass = [];
+
+
+	li.each(function(){
+		iClass.push( $(this).attr("class"));
+	 });
+
+	 var shuffledClasses = shuffle(iClass);
+
+	 $(".deck").find(".card").remove();
+
+	 for(var i=0; i<iClass.length; i++){
+
+		 var liClass =  iClass[i] ;
+		 var listItem = `<li class="card"><i class ="${liClass}"></i></li>`;
+		 $(".deck").append(listItem);
+	 }
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -24,8 +52,86 @@ function shuffle(array) {
 
     return array;
 }
+var matchedArray = [];
+
+function match(){
 
 
+	$(".deck").find(".show").toggleClass("show open").addClass("match");
+
+
+	matchedArray.push(testArr[0]);
+	matchedArray.push(testArr[1]);
+	testArr = [];
+}
+
+
+
+function unmatch(){
+
+	$(".deck").find(".show").toggleClass("show open");
+	testArr = [];
+
+}
+
+var testArr = [];
+function logic(){
+
+		var className = $(this).attr("class");
+		var regEx = /open/gi;
+
+		if(regEx.test(className)){
+			return true;
+		}
+
+		$(this).addClass("open");
+		$(this).addClass("show");
+
+		testArr.push($(this));
+
+		if(testArr.length>1){
+
+				moves++;
+				$(".moves").text(moves);
+
+				if(moves > 8 && moves < 16){
+					$(".stars").children().first().remove();
+				}
+				else if(moves>16 && moves <22){
+					$(".stars").children().first().remove();
+				}else{
+					return true;
+				}
+
+
+
+
+
+
+				if(testArr[0][0]["childNodes"][0]["className"] === testArr[1][0]["childNodes"][0]["className"] ){
+
+					//add class match
+					setTimeout(match(), 400);
+
+					//add both of them to a new array matched
+				}else{
+
+					setTimeout(unmatch, 400);
+					//remove class show and open
+					//remove them from testArr
+				}
+}
+
+	if(matchedArray.length == 16){
+			console.log("16")
+	}
+
+
+	};
+
+
+$(".deck").on("click", ".card", logic);
+};
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -36,3 +142,4 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+begin();

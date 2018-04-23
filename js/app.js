a9a782ac4a85cc3d36f831f3c568708fbaa2b3f0
Chris Camp <cpcamp2@gmail.com>
2018-04-23 17:14:43 -0400

    Create cards, shuffle and conditional logic to see if cards match or not

--- js/app.js
+++ js/app.js
@@ -1,14 +1,38 @@
 /*
  * Create a list that holds all of your cards
  */
-
-
+let symbols = ['diamond', 'paper-plane-o', 'anchor', 'bolt', 'cube', 'anchor', 'leaf', 'bicycle', 'diamond', 'bomb', 'leaf', 'bomb', 'bolt', 'bicycle', 'paper-plane-o', 'cube'];
+const deck = document.querySelector('.deck');
+const cards = deck.getElementsByTagName('li');
+let opened = [];
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+
+function startGame(){
+  // Shuffle symbols
+  let shuffleDeck = shuffle(symbols);
+
+  // Remove cards from previous game if any
+  while (deck.firstChild){
+    deck.removeChild(deck.firstChild);
+  }
+
+  // Loop through symbols and add cards to deck w/ icons
+  for (let i = 0; i < shuffleDeck.length; i++) {
+    let card = document.createElement('li');
+    card.className = "card";
+    let icon = document.createElement('i');
+    icon.className = "fa fa-" + shuffleDeck[i];
+    card.appendChild(icon);
+    deck.appendChild(card);
+  }
+
+  cardListener();
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -36,3 +60,44 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+// Create function to add event listener to deck and delegate events to all cards
+const cardListener = function(){
+  deck.addEventListener('click', function(e){
+    let card = e.target;
+
+    if (card.tagName != 'LI') return;
+
+    // If card has class name show or match return true
+    if (card.className === 'card open show' || card.className === 'card match' ) {
+      return true;
+    }
+
+    // If card is true then push into opened array
+  if (card) {
+      card.className += ' open show';
+      opened.push(card);
+  }
+    // Conditional that if opened has two items in array then run conditional to see if they match
+    if (opened.length > 1){
+      // If the items match then iterate through the cards in deck and change class names to card match
+      if (card.firstChild.className === opened[0].firstChild.className) {
+        for (let x = 0; x < cards.length; x++) {
+          if (cards[x].className === 'card open show') {
+            cards[x].className = 'card match';
+          }
+        }
+      // If the items do not match iterate through the cards and change class names back to just card add a delay so the user can see the second card they click on for a moment
+      } else {
+        setTimeout(function (){
+          for (let x = 0; x < cards.length; x++) {
+            if (cards[x].className === 'card open show') {
+              cards[x].className = 'card';
+            }
+          }
+        }, 500);
+      };
+      opened = [];
+    }
+  });
+}

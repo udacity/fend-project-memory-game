72063f9c3d0d853740e659e943e780ed5ef2218d
americanman <snowboardracer7@gmail.com>
2018-05-21 14:10:47 -0400

    Refresh functionality added

--- js/app.js
+++ js/app.js
@@ -2,6 +2,23 @@
  * Create a list that holds all of your cards
  */
 
+//works with numbers
+var num = [8,3,4,5,9];
+var restart = document.getElementsByClassName('restart');
+restart[0].addEventListener('click', function() {
+    console.log(num);
+    console.log('clicked');
+    console.log(shuffle(num));
+});
+
+//doesn't work with HTMLcollection
+var list = document.getElementsByClassName('card');
+var restart = document.getElementsByClassName('restart');
+restart[0].addEventListener('click', function() {
+    console.log(list);
+    console.log('clicked with HTMLcollection');
+    console.log(shuffle(list));
+});
 
 /*
  * Display the cards on the page
@@ -9,22 +26,25 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
 
-    return array;
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
-
 
 /*
  * set up the event listener for a card. If a card is clicked:

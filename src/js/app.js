2fecfa40186cb9198f2b5edbda9231538ebed0b2
aditya81070 <aditya81070@gmail.com>
2018-08-05 00:29:25 +0530

    Add finalscore, starScore function

--- src/js/app.js
+++ src/js/app.js
@@ -58,19 +58,20 @@ moves.textContent = moveCounter + ' Moves';
 let timerId = 0;
 let timerOn = false;
 
+restartButton.addEventListener('click',function(){
+	restart(deck,ul);
+});
+
 deck.addEventListener('click', function(e) {
 	let minuteCounter = 0;
 	let secondCounter = 0;
 
-	if (matchedCards.length == 16) {
+	if (matchedCards.length === 16) {
 		timerOn = false;
 		clearInterval(timerId);
-	}
-	if (timerOn) {
 		return;
 	}
-
-	if (e.target.classList.contains('card')) {
+	if (e.target.classList.contains('card')&& !timerOn) {
 		timerOn = true;
 		min.textContent = '00';
 		sec.textContent = '00';
@@ -88,63 +89,136 @@ deck.addEventListener('click', function(e) {
 		secondCounter++;
 		sec.textContent = secondCounter < 10 ? '0' + secondCounter : secondCounter;
 	}
-
-	for (let card of cards) {
-		card.addEventListener('click', show);
-	}
-
-	function show(e) {
-		console.log('this is click');
-		if (opendCards.length >= 2 || e.target.classList.contains('open', 'show') || e.target.classList.contains('match')) {
-			return;
-		}
-
-		e.target.classList.add('open', 'show', 'animated', 'flipInY');
-		opendCards.push(e.target);
-		if (opendCards.length == 2) {
-			moveCounter++;
-			moves.textContent = moveCounter === 1 ? 1 + ' Move' : moveCounter + ' Moves';
-			// starScore('.stars','inline-block');	
-			match();
-		}
-	}
-
-	function match() {
-		if (opendCards[0].firstElementChild.getAttribute('class') === opendCards[1].firstElementChild.getAttribute('class')) {
-			opendCards.map(function(card) {
-				card.className = 'card match animated tada';
-				matchedCards.push(card);
-			});
-			// setTimeout(finalScore,500);
-			opendCards = [];
-		} else {
-			setTimeout(function() {
-				for (let opendcard of opendCards) {
-					setTimeout(function() {
-						opendcard.classList.replace('flipInY', 'headShake');
-						opendcard.classList.add('card-not-match');
-						opendcard.classList.remove('open');
-					}, 500);
-
-				}
-				setTimeout(function() {
-					console.log('close');
-					console.log(opendCards);
-					for (let opendcard of opendCards) {
-						// opendcard.classList.replace('headShake','flipInY');
-						opendcard.classList.remove('open', 'show', 'card-not-match');
-						opendcard.classList.remove('flipInY', 'animated');
-						console.log(opendcard);
-					}
-					opendCards = [];
-				}, 800);
-
-			}, 900);
-
-		}
-	}
-
 });
+
+//reveal cards;
+for (let card of cards) {
+	card.addEventListener('click', show);
+}
+
+function show(e) {
+	console.log('this is click');
+	if (opendCards.length >= 2 || e.target.classList.contains('open', 'show') || e.target.classList.contains('match')) {
+		return;
+	}
+
+	e.target.classList.add('open', 'show', 'animated', 'flipInY');
+	opendCards.push(e.target);
+	if (opendCards.length == 2) {
+		moveCounter++;
+		moves.textContent = moveCounter === 1 ? 1 + ' Move' : moveCounter + ' Moves';
+		starScore('.stars');	
+		match();
+	}
+}
+
+function match() {
+	if (opendCards[0].firstElementChild.getAttribute('class') === opendCards[1].firstElementChild.getAttribute('class')) {
+		opendCards.map(function(card) {
+			card.className = 'card match animated tada';
+			matchedCards.push(card);
+		});
+		setTimeout(finalScore, 500);
+		opendCards = [];
+	} else {
+		setTimeout(function() {
+			for (let opendcard of opendCards) {
+				setTimeout(function() {
+					opendcard.classList.replace('flipInY', 'headShake');
+					opendcard.classList.add('card-not-match');
+					opendcard.classList.remove('open');
+				}, 500);
+
+			}
+			setTimeout(function() {
+				console.log('close');
+				console.log(opendCards);
+				for (let opendcard of opendCards) {
+					// opendcard.classList.replace('headShake','flipInY');
+					opendcard.classList.remove('open', 'show', 'card-not-match');
+					opendcard.classList.remove('flipInY', 'animated', 'headShake');
+					console.log(opendcard);
+				}
+				opendCards = [];
+			}, 800);
+
+		}, 900);
+
+	}
+}
+
+function finalScore() {
+	const playAgain = document.querySelector('#modal-playagain');
+	const minFinal = document.querySelector('.min');
+	const secFinal = document.querySelector('.sec');
+	const timeFinal = document.querySelector('#total-time');
+
+	playAgain.addEventListener('click', function() {
+		restart(deck, ul);
+		$('#exampleModalLong').modal('hide');
+	});
+
+	if (matchedCards.length === 16) {
+		$('#exampleModalLong').modal({
+			backdrop: 'static',
+			keyboard: false
+		});
+		console.log('this is working kfjlakjdlfa');
+		timeFinal.textContent = minFinal.textContent + ' min ' + secFinal.textContent + ' sec';
+		starScore('.modal-stars');
+
+	}
+}
+
+function restart(parentTag, fragment) {
+	const newCardArr = [];
+	cards.forEach(function(card) {
+		newCardArr.push(card);
+	});
+	parentTag.innerHTML = '';
+	cards = shuffle(newCardArr);
+	for (let card of cards) {
+		fragment.appendChild(card);
+	}
+	parentTag.appendChild(fragment);
+
+	if (opendCards.length === 0 && matchedCards.length === 0 && moveCounter === 0) {
+		return;
+	}
+	matchedCards.map(function(card) {
+		card.classList.remove('match', 'animated', 'tada');
+	});
+	opendCards.map(function(card) {
+		card.classList.remove('open', 'show');
+	});
+	moveCounter = 0;
+	moves.textContent = moveCounter + ' Moves';
+	min.textContent = '00';
+	sec.textContent = '00';
+	opendCards = [];
+	matchedCards = [];
+	starScore('.stars');
+	timerOn = false;
+	clearInterval(timerId);
+}
+
+function starScore(selector) {
+	const stars= document.querySelector(selector).children;
+	const comment=document.querySelector('.comment');
+	if(moveCounter<=12){
+		comment.textContent='Awesome!';
+	}
+	else if (moveCounter>12 && moveCounter<20){
+		stars[2].firstElementChild.className='hide';
+		comment.textContent='Good!';
+	}
+	else{
+		stars[1].firstElementChild.className='hide';
+		stars[2].firstElementChild.className='hide';
+		comment.textContent='You can do it better';
+	}
+}
+
 
 /*
  * set up the event listener for a card. If a card is clicked:

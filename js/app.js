47e3a5f07212e9a55fe6ad59ca85a1617b71fdad
Teba Gomez <gomez.teba@gmail.com>
2018-12-16 15:13:26 +0000

    clean up and sanitization for MVP release. Related #13

--- js/app.js
+++ js/app.js
@@ -214,16 +214,3 @@ function shuffle(array) {
 
     return array;
 }
-
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */

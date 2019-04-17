51d25c472ac9b4a77e4b804fa710f71ce16884de
Daria Prokofieva <dashaprokofieva95@gmail.com>
2019-04-17 13:16:10 +0200

    Add EventListener

--- js/app.js
+++ js/app.js
@@ -36,3 +36,9 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+const allCards=document.querySelectorAll ('.card')
+allCards.forEach(function(cards){
+   cards.addEventListener('click',function(){
+      cards.classList.add('open','show');
+  });
+});
